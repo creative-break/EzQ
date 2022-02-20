24df20ead17651e212599d07f59fce263babe72a
Rune <rbohns18@student.aau.dk>
2022-02-20 20:04:07 +0100

    GUI work

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -181,8 +181,8 @@ bool EzQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* EzQAudioProcessor::createEditor()
 {
-    //return new EzQAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new EzQAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -191,12 +191,21 @@ void EzQAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void EzQAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
